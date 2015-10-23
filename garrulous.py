e017117b083424456eb37ce5566a21915e49ba80
Richard Meyers <remsolutions2010@gmail.com>
2015-10-22 23:57:27 -0400

    Added SQLite support

--- garrulous.py
+++ garrulous.py
@@ -2,6 +2,7 @@ __author__ = 'Richard Meyers'
 
 import os
 import cherrypy
+import sqlite3
 import json
 
 class SiteIndex(object):
@@ -32,22 +33,33 @@ class UserApi(object):
 
     @cherrypy.tools.json_out()
     def GET(self):
-        return "User NO"
+        return {'error': True, 'msg': "Error during request"}
 
     @cherrypy.tools.json_out()
     def POST(self):
-        return "User NO"
-
+        return {'error': True, 'msg': "Error during request"}
 
     @cherrypy.tools.json_out()
     def PUT(self):
-        return False
+        return {'error': True, 'msg': "Error during request"}
 
     @cherrypy.tools.json_out()
     def DELETE(self):
-        return False
+        return {'error': True, 'msg': "Error during request"}
 
 if __name__ == '__main__':
+    #get SQLite started.
+    conn = sqlite3.connect('database.db')
+    #The database needs to be queried here. If the
+    # tables don't exist they need to be created.
+    # Add the schema here.
+    db_cursor = conn.cursor()
+
+    #This is just an example
+    """db_cursor.execute("CREATE TABLE IF NOT EXISTS `abuses` (" +
+                      "`abuse_id` int(11) NOT NULL AUTO_INCREMENT," +
+                      "`user_id` int(11) NOT NULL DEFAULT '0',")"""
+
     conf = {
         '/': {
             'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
